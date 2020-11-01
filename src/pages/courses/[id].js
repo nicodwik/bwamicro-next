7cd6f8f65fe08ef469f9dea568973a6a4999ff3c
nicodwik <nicodwika@gmail.com>
2020-11-01 11:28:15 +0700

    add sticky price

---
+++ src/pages/courses/[id].js
@@ -0,0 +1,146 @@
+import {useRef, useState, useEffect} from 'react'
+import Head from 'next/head'
+import courses from 'src/constans/api/courses'
+import Youtube from 'react-youtube'
+import Header from 'src/parts/Header'
+import Feature from 'src/parts/Details/Feature'
+import {CSSTransition} from 'react-transition-group'
+import formatThousand from 'src/helpers/formatThousand' 
+import Footer from 'src/parts/Footer'
+
+//public images
+import Student from 'public/images/icon-student.svg'
+import Video from 'public/images/icon-video.svg'
+import Certificate from 'public/images/icon-certificate.svg'
+
+export default function DetailCourse({data}) {
+    console.log(data)
+
+    const footer = useRef(null)
+    const [isSticky, setIsSticky] = useState(() => true)
+    useEffect(() => {
+        const stickyOffestTop = footer.current.getBoundingClientRect().top
+        const stickyMetaToogler = () => {
+            console.log(stickyOffestTop, window.pageYOffset, window.innerHeight)
+            setIsSticky(stickyOffestTop >= window.pageYOffset + window.innerHeight)
+        }
+
+        window.addEventListener("scroll", stickyMetaToogler)
+        return () => {
+            window.removeEventListener("scroll", stickyMetaToogler)
+        }
+    }, [])
+    return (
+        <>
+            <Head>
+                <title>Micro</title>
+            </Head>
+
+            <section className="pt-10 relative overflow-hidden" style={{height:660}}>
+                {
+                    data?.chapters?.[0]?.lessons?.[0]?.video &&
+                    <div className="video-wrapper">
+                        <Youtube videoId={data?.chapters?.[0]?.lessons?.[0]?.video}
+                        id={data?.chapters?.[0]?.lessons?.[0]?.video}
+                        opts={{
+                            playerVars: {
+                                loop: 1,
+                                mute: 1,
+                                autoplay: 1,
+                                controls: 0,
+                                showinfo: 0
+                            }
+                        }}
+                        onEnd={(event) => event.target.playVideo()} 
+                        ></Youtube>
+                    </div>
+                }
+
+                <div className="absolute inset-0 z-0 w-full h-full bg-black opacity-75">
+                    <div className="meta-title absolute inset-0 object-fill z-0 w-full flex justify-center items-center">
+                        <div className="text-center">
+                            <h3 className="text-lg text-white">Kelas Online: </h3>
+                            <h4 className="text-6xl text-teal-500 font-semibold">
+                                {
+                                    data?.name ?? "Nama Kelas"
+                                }
+                            </h4>
+                        </div>
+                    </div>
+                    <div className="container z-10 mx-auto relative">
+                        <Header></Header>
+                    </div>
+                </div>
+            </section>
+            <section className="container mx-auto pt-24 relative">
+                <div className="absolute top-0 w-full transform -translate-y-1/2">
+                    <div className="w-3/4 mx-auto">
+                        <div className="flex justify-between">
+                            <Feature data={{
+                                icon: <Student className="fill-teal-500"/>,
+                                meta: "Student",
+                                value: data?.total_student ?? 0
+                            }} />
+                            <Feature data={{
+                                    icon: <Video className="fill-teal-500" />,
+                                    meta: "Video",
+                                    value: data?.total_videos ?? 0
+                            }} />
+                            <Feature data={{
+                                icon: <Certificate className="fill-teal-500" />,
+                                meta: "Certificate",
+                                value: data.certificate === 1 ? "Tersedia" : "Tidak Tersedia"
+                            }} />
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <CSSTransition in={isSticky} timeout={300}  classNames="meta-price" unmountOnExit>
+                        <div className="meta-price w-full bg-white z-50 left-0 py-3">
+                            <div className="w-3/4 mx-auto">
+                                <div className="flex items-center">
+                                    <div className="w-full">
+                                        <h2 className="text-gray-600">Nama Kelas</h2>
+                                        <h3 className="text-2xl text-gray-900">{data?.name ?? "Nama Kelas"} </h3>
+                                    </div>
+                                    <h5 className="text-2xl text-teal-500 whitespace-no-wrap mr-4">
+                                        {data?.type === "free" ? "Free" : <span> Rp.{formatThousand(data?.price ?? 0)} </span> }
+                                    </h5>
+                                    <a href={`${process.env.NEXT_PUBLIC_MEMBERPAGE_URL}/joined/${data.id}`} target="_blank" rel="noopener noreferrer" 
+                                    className="bg-orange-500 hover:bg-orange-400 transition-all duration-200 focus:outline-none shadow-inner text-white
+                                    px-6 py-3 whitespace-no-wrap">
+                                        {data?.type === "free" ? "Enroll Now" : "Buy Now"}
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </CSSTransition>
+                </div>
+            </section>
+            <div className="w-3/4 mx-auto mt-8">
+                <div className="w-3/4">
+                    <section>
+                        <h6 className="font-medium text-gray-900 text-2xl mb-4">About <span className="text-teal-500">Course</span></h6>
+                        <p className="text-gray-600 text-lg leading-relaxed mb-3">
+                            {data?.description ?? "No Description Found"}
+                        </p>
+                    </section>
+                </div>
+            </div>
+            <div style={{height: 2000}}></div>
+            <section className="mt-24 bg-indigo-1000 py-12" ref={footer}>
+                <Footer></Footer>
+            </section>
+        </>
+    )
+}
+
+DetailCourse.getInitialProps = async (props) => {
+    const { id } = props.query
+    try {
+        const data = await courses.details(id)
+        return { data }
+    } catch (error) {
+        
+    }
+}
--- src/parts/ListCourses/RenderItem.js
+++ src/parts/ListCourses/RenderItem.js
@@ -3,7 +3,6 @@ import Link from 'next/link'
 import IconPlay from 'public/images/btn_play.svg'
 
 export default function RenderItem({item}) {
-    console.log(item)
     return (
         <div className='w-1/4 px-4'>
             <div className="item relative">
@@ -15,7 +14,7 @@ export default function RenderItem({item}) {
                     <h4 className="text-md text-gray-900">{item?.name ?? 'Course Name'} </h4>
                     <h5 className="text-sm text-gray-600">{item?.level ?? 'Course Level'} </h5>
                 </div>
-                <Link href="/course/[slug]" as={`/course/${item.id}`}><a className="link-wrapped"></a></Link>
+                <Link href="/courses/[slug]" as={`/courses/${item.id}`}><a className="link-wrapped"></a></Link>
             </div>
         </div>
     )
